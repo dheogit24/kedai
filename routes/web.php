911e995bae312f0af99b627716cb794d54456cb9
deo rafael <dhewrfhel@gmail.com>
2024-10-16 13:47:43 +0800

    git init laravel

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route halaman login
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.auth-login');
 });
 
-Route::get('/dashboard', function () {
-    return view('pages.dashboard', ['type_menu' => 'dashboard']);
+// Route halaman register (tidak perlu middleware auth)
+Route::get('register', function () {
+    return view('auth.auth-register', ['type_menu' => 'register']);
+})->name('register');
+
+// Route yang memerlukan autentikasi (hanya bisa diakses jika sudah login)
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard', ['type_menu' => 'home']);
+    })->name('home');
 });
